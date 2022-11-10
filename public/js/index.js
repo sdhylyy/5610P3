3fbb37246cc75e79325eea630f9859aa407d7063
lyx <li.yunxi@northeastern.edu>
2022-11-09 19:11:12 -0800

    P2 files

---
+++ public/js/index.js
@@ -0,0 +1,37 @@
+//const deployURL="http://localhost:3000/";
+const deployURL="https://intense-lowlands-69751.herokuapp.com/";
+
+const loginURL = deployURL+'api/login/';
+
+document.getElementById("login-form").action =loginURL;
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+function checkMsg(){
+    let msg=urlParams.get("msg");
+    if(msg){
+        alert(msg);
+    }
+}
+window.onload = () => {
+    checkMsg();
+};
+
+function checkForm(){
+    let name = document.getElementById("username").value;　
+    console.log(name);
+    let password = document.getElementById("password").value;　
+    console.log(password);
+    if(!name||name==""){
+        alert("username mustn't be empty.");
+        return false;
+    }
+    if(!password||password==""){
+        alert("password mustn't be empty.");
+        return false;
+    }
+
+
+　　return true;
+}
---
+++ public/js/register.js
@@ -0,0 +1,44 @@
+//const deployURL="http://localhost:3000/";
+const deployURL="https://intense-lowlands-69751.herokuapp.com/";
+
+const registerURL = deployURL+'api/register/';
+
+document.getElementById("register-form").action =registerURL;
+console.log(document.getElementById("register-form").action);
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+function checkMsg(){
+    let msg=urlParams.get("msg");
+    if(msg){
+        alert(msg);
+    }
+}
+window.onload = () => {
+    checkMsg();
+  };
+
+function checkForm(){
+    let name = document.getElementById("username").value;　
+    console.log(name);
+    let password = document.getElementById("password").value;　
+    console.log(password);
+    let position = document.getElementById("position").value;　
+    console.log(position);
+    if(!name||name==""){
+        alert("username mustn't be empty.");
+        return false;
+    }
+    if(!password||password==""){
+        alert("password mustn't be empty.");
+        return false;
+    }
+    if(!position||position==""){
+        alert("position mustn't be empty.");
+        return false;
+    }
+
+
+　　return true;
+}
+
